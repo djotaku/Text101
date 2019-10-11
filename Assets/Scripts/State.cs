79cff5885c520ed5c7e8e6a69b20e7fceb1723e5
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-10 21:27:08 -0400

    Organized the scripts and states into folders. Created states for the rest of the game. Fill states with the course's supplied text.

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -7,9 +7,15 @@ public class State : ScriptableObject
 {
     // Start is called before the first frame update
     [TextArea(10,14)] [SerializeField] string storyText; //TextArea allows you to have more space in unity inspector when writing. First number is min size, sec number is amount of lines before scroll
+    [SerializeField] State[] nextStates; //by serailizing we can do this in the Unity GUI
 
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
     }
 }
